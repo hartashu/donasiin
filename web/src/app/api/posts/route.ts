2e22759011256060a276768a0ce3dcbf406c90c1
hartashu <harta.shuwanto@gmail.com>
2025-07-24 11:36:27 +0700

    feat: Make types.ts file for typescript types

---
+++ web/src/app/api/posts/route.ts
@@ -0,0 +1,13 @@
+export async function GET(_request: Request) {
+  // return Response.json({
+  //   data: "haha",
+  // });
+}
+
+export async function POST(request: Request) {}
+
+export async function PUT(request: Request) {}
+
+export async function DELETE(request: Request) {}
+
+export async function PATCH(request: Request) {}
