8e84753a6a147b3e7e2144a1ae5c713b8bc15f8d
Rexdust <andy0412lie@gmail.com>
2025-07-24 14:14:44 +0700

    Update: login, register, & home page

--- web/src/app/auth/register/page.tsx
+++ web/src/app/auth/register/page.tsx
@@ -1,9 +1,10 @@
-import { RegisterForm } from '@/components/auth/RegisterForm'
+import { RegisterForm } from "@/components/auth/RegisterForm";
 
 export default function RegisterPage() {
-    return (
-        <div className="min-h-screen bg-gray-50 flex items-center justify-center p-4">
-            <RegisterForm />
-        </div>
-    )
+  return (
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center p-4 flex-col">
+      <div className="mb-4 text-3xl">[LOGO] Donasiin</div>
+      <RegisterForm />
+    </div>
+  );
 }
--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { User, Plus, MessageCircle, House } from "lucide-react";
+import ClientLayout from "@/components/ClientLayout";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,40 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning={true}
       >
-        <main className="bg-white text-black">
-          <nav className="border-b border-b-gray-300 border-bol flex justify-between items-center p-3 pl-8 pr-8">
-            <div className="text-2xl font-bold cursor-pointer">Donasiin</div>
-            <div className="flex gap-2 items-center cursor-pointer">
-              <div>
-                <House className="w-5" />
-              </div>
-              <div className="text-md">Discover</div>
-            </div>
-
-            <div className="flex gap-8 items-center cursor-pointer">
-              <div className="flex gap-2">
-                <div>
-                  <MessageCircle className="w-5" />
-                </div>
-                <div>Messages</div>
-              </div>
-
-              <div className="bg-green-800/80 px-2 py-1 rounded-md">
-                <div className="flex gap-2 cursor-pointer ">
-                  <div className="text-white font-bold">
-                    <Plus className="w-4 " />
-                  </div>
-                  <div className="text-white font-semibold">Share Item</div>
-                </div>
-              </div>
-
-              <div className="cursor-pointer">
-                <User className="w-6" />
-              </div>
-            </div>
-          </nav>
-          <section className="min-h-screen">{children}</section>
-        </main>
+        <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   );
